3cc0f3fba898c148bd18278420be34653c0b7c9f
Nguyen Hoang Khai <nguyenhoangkhai2003tgl@gmail.com>
2023-03-10 20:31:54 +0700

    ngay 10/3/23 _ buoi toi ( delegate)

---
+++ Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿// delegate (type) bien = phuong thuc
+
+public delegate void ShowLog(string message);
+class Program
+{
+    static void Info(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(s);
+        Console.ResetColor();
+    }
+    static void Warning(string s)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(s);
+        Console.ResetColor();
+    }
+    static void Main(string[] args) {
+        ShowLog log = null; // +=
+      //  log = Info;
+        log += Info;
+        log += Warning;
+        log?.Invoke("Nguyen Hoang Khai");
+    }
+}
