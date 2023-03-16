0dbceff3636c6f172206a15b156b5ebb2660f41d
Nguyen Hoang Khai <nguyenhoangkhai2003tgl@gmail.com>
2023-03-16 10:43:33 +0700

    drive , queue, stack ,..(ngay 16/3/2023)

---
+++ FileExample/Program.cs
@@ -0,0 +1,34 @@
+﻿// doc file
+class Program
+{
+    static void Main(string[] args)
+    {
+        //DriveInfo drive = new DriveInfo("/");
+        //driveinfo => lay thong tin cua o dia trong may
+        //var drives = DriveInfo.GetDrives();
+        //foreach(var drive in drives)
+        //{
+
+        //    Console.WriteLine($"Drive:{drive.Name}");
+        //    Console.WriteLine($"Drive:{drive.DriveType}");
+        //    Console.WriteLine($"Drive:{drive.VolumeLabel}");
+        //    Console.WriteLine($"Drive:{drive.DriveFormat}");
+        //    Console.WriteLine($"Drive:{drive.TotalSize}");
+        //    Console.WriteLine($"Drive:{drive.TotalFreeSpace}");
+        //    Console.WriteLine("=========================");
+        //}
+
+        //-----------------------
+        //Directory - thu muc
+        string path = "nguyenhoangkhai";
+        // tao ra thu muc
+       // Directory.CreateDirectory(path);
+        // xoa thu muc
+        Directory.Delete(path);
+        if (Directory.Exists(path))
+        {
+            Console.WriteLine($"{path} ton tai");
+        }else { Console.WriteLine($"{path} khong ton tai"); }
+        
+    }
+}
---
+++ Queue_Stack_Linkedlist/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Queue => vao trc ra truoc
+        //Queue<string> queue = new Queue<string>();
+        //queue.Enqueue("ho so 1");
+        //queue.Enqueue("ho so 2");
+        //queue.Enqueue("ho so 3");
+
+        //foreach(var item in queue)
+        //{
+        //    Console.WriteLine(item);
+        //}
+        //// phan tu nao co truoc thi su ly trc
+        //var hoso = queue.Dequeue();
+        //Console.WriteLine($"Xu ly ho so: {hoso} ");
+        //hoso = queue.Dequeue();
+        //Console.WriteLine($"Xu ly ho so: {hoso} ");
+        //hoso = queue.Dequeue();
+        //Console.WriteLine($"Xu ly ho so: {hoso} ");
+
+        // stack : nguoc lai vs Queue, cai nao vao trc thi xuong duoi, nao vao sau thi dc lay ra trc => vao sau thi ra trc
+        //Stack<string> pro = new Stack<string>();
+        //pro.Push("a");
+        //pro.Push("b");
+        //pro.Push("c");
+        //pro.Push("d");
+
+        //var mathang = pro.Pop();
+        //Console.WriteLine($"Lay hang ra:{mathang}");
+
+        // LinkedList => danh sach lien ket
+        LinkedList<string> list = new LinkedList<string>();
+        list.AddFirst("a");
+        list.AddFirst("b");
+        list.AddLast("c");
+        foreach(var s in list)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
